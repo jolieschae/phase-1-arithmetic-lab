2aed07554e6331b43fd2863d6080f71850f41bb3
Jolie Gielchinsky <jolieschae@gmail.com>
2023-03-21 10:37:22 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+const num1 = 2
+const num2 = 31
+
+const multiply = num1 * num2
+
+
+
+const random = Math.floor(Math.random()* 10)
+
+const num3 = 36
+const num4 = 8
+
+const mod = num3 % num4
+
+const max = Math.max(2, 6, 12, 18, 20);
